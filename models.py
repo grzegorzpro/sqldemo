6d924f9560b4258c5576c09d8ab85cca599dbd40
Grzegorz Prokopczyk <grzegorz.prokopczyk@f-secure.com>
2018-02-13 17:45:26 +0100

    Split models and setup_db

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -14,28 +12,3 @@ class BlogPost(db.Model):
 
     def __repr__(self):
         return "BlogPost(id={}, title='{}', author='{}'".format(self.id, self.title, self.author)
-
-
-if __name__ == '__main__':
-    from app import app
-
-    app.app_context().push()
-    db.init_app(app)
-
-    db.create_all()
-
-    post1 = BlogPost(
-        title='Welcome to PyLove!',
-        content='Let\'s learn how to write web apps ;)',
-        author='Hiromi Uehara',
-        created=datetime(2018, 2, 7)
-    )
-    post2 = BlogPost(
-        title='Example post',
-        content='Example post content',
-        author='Aaron Parks',
-        created=datetime(2018, 2, 10)
-    )
-    db.session.add(post1)
-    db.session.add(post2)
-    db.session.commit()
---
+++ setup_db.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from app import app, db
+from models import BlogPost
+
+app.app_context().push()
+
+db.create_all()
+
+post1 = BlogPost(
+    title='Welcome to PyLove!',
+    content="Let's learn how to write web apps ;)",
+    author='Hiromi Uehara',
+    created=datetime(2018, 2, 7)
+)
+post2 = BlogPost(
+    title='Example post',
+    content='Example post content',
+    author='Aaron Parks',
+    created=datetime(2018, 2, 10)
+)
+db.session.add(post1)
+db.session.add(post2)
+db.session.commit()
